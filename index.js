d3ce263720dbf631ef749427810bc7ed3bfd3554
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-07-11 16:48:05 +0530

    Added basic structure
    Includes index.js, stats routes page and view, public directory

---
+++ index.js
@@ -0,0 +1,26 @@
+//NODE PACKAGES
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const path = require('path');
+
+const stats = require('./routes/stats');
+const leads = require('./routes/leads');
+
+//-----------------------------------------------------------------------------------------------------------//
+//MIDDLEWARE
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/stats', stats);
+app.use('/leads', leads);
+//----------------------------------------------------------------------------------------------------------//
+
+
---
+++ routes/leads.js
@@ -0,0 +1,8 @@
+
+//THIS PAGE IS SOLELY DEDICATED TOWARDS LEADS 
+
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = router;
