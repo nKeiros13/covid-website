012329f1a9f2f5a1139733157b042a22ffb3bf8b
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-07-11 17:13:27 +0530

    added boilerplate.js, updated index.js and stats.js.

--- routes/stats.js
+++ routes/stats.js
@@ -4,5 +4,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req,res)=>{
+    res.send("You're viewing STATS PAGE!");
+})
+router.get('/:cName', (req,res)=>{
+    const { cName } = req.params;
+    res.send(`You're viewing STATS page for ${cName}.`);
+})
+
 
 module.exports = router;
