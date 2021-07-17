d07e15dd227884fef3eb2227a6538fd75f675b9e
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-07-17 21:14:15 +0530

    updated stats.js
    updated stats.js so that it can fetch data from a covid api and then send the parsed data to be rendered in stats/index.ejs webpage.

--- routes/stats.js
+++ routes/stats.js
@@ -3,10 +3,18 @@
 
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/', (req,res)=>{
+
+
+
+router.get('/', async (req,res)=>{
     //THIS PAGE DISPLAYS THE INDEX
-    res.render('stats/index');
+    const rawInfo = await axios.get('https://api.covidtracking.com/v1/states/current.json');
+    console.log(rawInfo);
+    const Data = rawInfo.data;
+    res.render('stats/index', {Data});
+    
 })
 router.get('/:cName', (req,res)=>{
     const { cName } = req.params;
