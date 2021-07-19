03d52b7b4db61df1aaae9be55ef4c18f1109883c
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-07-19 10:24:47 +0530

    sending a json file to replace statecode with state name

--- routes/stats.js
+++ routes/stats.js
@@ -4,6 +4,21 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const State = require('../models/states');
+const states = require('../seeds/states');
+
+
+
+//ASYNC FUNCTION TO GET STATE NAME BY ABBREVIATION VIA DB
+// const getState = async (data) => {
+//     let states = [];
+//     for (let d of data) {
+//         const abv = d.state;
+//         states += await State.findOne({ stateCode: abv });
+//     }
+//     return states;
+// }
+
 
 
 // DONOT PREFIX ANY ROUTE WITH STATS. LOOK UP MAIN INDEX.JS 
@@ -11,11 +26,13 @@ const axios = require('axios');
 router.get('/', async (req,res)=>{
     //THIS PAGE DISPLAYS THE INDEX
     const rawInfo = await axios.get('https://api.covidtracking.com/v1/states/current.json');
-    console.log(rawInfo);
     const Data = rawInfo.data;
-    res.render('stats/index', {Data});
-    
+    res.render('stats/index', { Data, states });
 })
+
+
+
+
 router.get('/:cName', (req,res)=>{
     const { cName } = req.params;
     //THIS PAGE DISPLAYS THE STATS REGARDING A PARTICULAR COUNTRY. cName IS COUNTRY-NAME
