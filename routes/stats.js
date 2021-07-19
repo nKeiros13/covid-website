d543a0dd52c9c8790769970b9c5aec5fbdc4d0c7
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-07-19 12:38:21 +0530

    added custom routes for each state

--- routes/stats.js
+++ routes/stats.js
@@ -1,13 +1,10 @@
+//THIS PAGE IS SOLELY DEDICATED TOWARDS STATS
 
-//THIS PAGE IS SOLELY DEDICATED TOWARDS STATS 
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const State = require('../models/states');
-const states = require('../seeds/states');
-
-
+const axios = require("axios");
+const State = require("../models/states");
+const states = require("../seeds/states");
 
 //ASYNC FUNCTION TO GET STATE NAME BY ABBREVIATION VIA DB
 // const getState = async (data) => {
@@ -19,25 +16,43 @@ const states = require('../seeds/states');
 //     return states;
 // }
 
+//ASYNC FUNCTION TO GET DATA FROM COVID.
+// const getData = async () => {
+// 	const rawInfo = await axios.get(
+// 		"https://api.covidtracking.com/v1/states/current.json"
+// 	);
+// 	const Data = rawInfo.data;
+// 	return Data;
+// };
 
+// DONOT PREFIX ANY ROUTE WITH STATS. LOOK UP MAIN INDEX.JS
 
-// DONOT PREFIX ANY ROUTE WITH STATS. LOOK UP MAIN INDEX.JS 
+router.get("/", async (req, res) => {
+	//THIS PAGE DISPLAYS THE INDEX
+	const rawInfo = await axios.get(
+		"https://api.covidtracking.com/v1/states/current.json"
+	);
+	const Data = rawInfo.data;
+	res.render("stats/index", { Data, states });
+});
 
-router.get('/', async (req,res)=>{
-    //THIS PAGE DISPLAYS THE INDEX
-    const rawInfo = await axios.get('https://api.covidtracking.com/v1/states/current.json');
-    const Data = rawInfo.data;
-    res.render('stats/index', { Data, states });
-})
-
-
-
-
-router.get('/:cName', (req,res)=>{
-    const { cName } = req.params;
-    //THIS PAGE DISPLAYS THE STATS REGARDING A PARTICULAR COUNTRY. cName IS COUNTRY-NAME
-    res.send(`You're viewing STATS page for ${cName}.`);
-})
-
+router.get("/:sName", async (req, res) => {
+	let { sName } = req.params;
+	sName = sName.charAt(0).toUpperCase() + sName.slice(1); //Capitalize first letter.
+	const rawInfo = await axios.get(
+		"https://api.covidtracking.com/v1/states/current.json"
+	);
+	const Data = rawInfo.data;
+	for (let state of states) {
+		if (sName === state.name) {
+			for (let data of Data) {
+				if (state.abbreviation === data.state) {
+					res.render("stats/show", { data, state });
+				}
+			}
+			break;
+		}
+	}
+});
 
 module.exports = router;
