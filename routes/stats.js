a0e24d41bfad3ebff7c18db21a9c7c0cb1a64f29
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-07-22 22:48:15 +0530

    updated stats.js
    Moved covid api call to a separate function  called getData().

--- routes/stats.js
+++ routes/stats.js
@@ -3,46 +3,35 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const State = require("../models/states");
 const states = require("../seeds/states");
 
-//ASYNC FUNCTION TO GET STATE NAME BY ABBREVIATION VIA DB
-// const getState = async (data) => {
-//     let states = [];
-//     for (let d of data) {
-//         const abv = d.state;
-//         states += await State.findOne({ stateCode: abv });
-//     }
-//     return states;
-// }
-
 //ASYNC FUNCTION TO GET DATA FROM COVID.
-// const getData = async () => {
-// 	const rawInfo = await axios.get(
-// 		"https://api.covidtracking.com/v1/states/current.json"
-// 	);
-// 	const Data = rawInfo.data;
-// 	return Data;
-// };
+const getData = async () => {
+	try {
+		let rawInfo = await axios.get(
+			"https://api.covidtracking.com/v1/states/current.json"
+		);
+		console.log("Successfully got data!");
+		rawInfo = rawInfo.data;
+		return rawInfo;
+	} catch (e) {
+		console.log("Errors--");
+		console.log(e);
+	}
+};
 
 // DONOT PREFIX ANY ROUTE WITH STATS. LOOK UP MAIN INDEX.JS
 
 router.get("/", async (req, res) => {
 	//THIS PAGE DISPLAYS THE INDEX
-	const rawInfo = await axios.get(
-		"https://api.covidtracking.com/v1/states/current.json"
-	);
-	const Data = rawInfo.data;
+	const Data = await getData();
 	res.render("stats/index", { Data, states });
 });
 
 router.get("/:sName", async (req, res) => {
 	let { sName } = req.params;
 	sName = sName.charAt(0).toUpperCase() + sName.slice(1); //Capitalize first letter.
-	const rawInfo = await axios.get(
-		"https://api.covidtracking.com/v1/states/current.json"
-	);
-	const Data = rawInfo.data;
+	const Data = await getData();
 	for (let state of states) {
 		if (sName === state.name) {
 			for (let data of Data) {
