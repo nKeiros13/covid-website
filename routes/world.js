ae3772f3eba9bcc6a97b92fe07dcf40ee300832d
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-08-02 23:06:49 +0530

    added countries

--- routes/world.js
+++ routes/world.js
@@ -15,29 +15,18 @@ let options = {
 	},
 };
 
-let countryOptions = {
-	method: "GET",
-	url: "https://covid-19-data.p.rapidapi.com/country",
-	params: { name: "italy" },
-	headers: {
-		"x-rapidapi-key": "7031c91018msh98fbccce7eac202p1cda26jsn5823e0a1f254",
-		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-	},
-};
-
-axios
-	.request(options)
-	.then(function (response) {
-		console.log(response.data);
-	})
-	.catch(function (error) {
-		console.error(error);
-	});
-
-const getCountryDataByName = async () => {
+const getCountryDataByName = async (cName) => {
+	let countryOptions = {
+		method: "GET",
+		url: "https://covid-19-data.p.rapidapi.com/country",
+		params: { name: cName },
+		headers: {
+			"x-rapidapi-key": "7031c91018msh98fbccce7eac202p1cda26jsn5823e0a1f254",
+			"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		},
+	};
 	try {
 		let rawInfo = await axios.request(countryOptions);
-		console.log(rawInfo);
 		return rawInfo.data;
 	} catch (e) {
 		console.log("Error--");
@@ -57,15 +46,14 @@ const getData = async () => {
 // DONOT PREFIX ANY ROUTE WITH WORLD. LOOK UP MAIN INDEX.JS
 router.get("/", async (req, res) => {
 	const Data = await getData();
-	console.log(Data);
-	res.render("world/index", { Data });
+	console.log(countries);
+	res.render("world/index", { Data, countries });
 });
 
 router.get("/:cName", async (req, res) => {
 	let { cName } = req.params;
 	cName = cName.charAt(0).toUpperCase() + cName.slice(1); //Capitalize first letter.
-	console.log(cName);
-	let Data = await getCountryDataByName();
+	let Data = await getCountryDataByName(cName);
 	res.render("world/show", { Data });
 });
 
